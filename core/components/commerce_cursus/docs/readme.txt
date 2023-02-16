fbbbb38f9ea651393c7882a507a785a93fd44ff0
Mark Hamstra <hello@markhamstra.com>
2023-02-16 01:31:03 +0100

    Bootstrap extension

---
+++ core/components/commerce_cursus/docs/readme.txt
@@ -0,0 +1,4 @@
+Cursus for Commerce
+-------------------
+
+Connect Commerce with Cursus to confirm event registrations when payment is confirmed.
---
+++ core/components/commerce_cursus/lexicon/en/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_cursus'] = 'Cursus';
+$_lang['commerce_cursus.description'] = 'Connect Commerce with Cursus to confirm event registrations when payment is confirmed.';
