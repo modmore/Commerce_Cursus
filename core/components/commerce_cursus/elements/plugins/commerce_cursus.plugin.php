9a483ddccb4c6803989660cd3fee2b0ac849ec20
Thomas Jakobi <thomas.jakobi@partout.info>
2024-05-17 15:20:07 +0200

    Autoload the plugin event classes directly
    instead of doing this in the now deleted service class.

--- core/components/commerce_cursus/elements/plugins/commerce_cursus.plugin.php
+++ core/components/commerce_cursus/elements/plugins/commerce_cursus.plugin.php
@@ -12,22 +12,17 @@
 $className = 'modmore\Commerce_Cursus\Plugins\Events\\' . $modx->event->name;
 
 $corePath = $modx->getOption('commerce_cursus.core_path', null, $modx->getOption('core_path') . 'components/commerce_cursus/');
-/** @var Commerce_Cursus $commerce_cursus */
-$commerce_cursus = $modx->getService('commerce_cursus', 'Commerce_Cursus', $corePath . 'model/commerce_cursus/', [
-    'core_path' => $corePath
-]);
+require_once $corePath . '/vendor/autoload.php';
 
-if ($commerce_cursus) {
-    if (class_exists($className)) {
-        $handler = new $className($modx, $scriptProperties);
-        if (get_class($handler) == $className) {
-            $handler->run();
-        } else {
-            $modx->log(xPDO::LOG_LEVEL_ERROR, $className. ' could not be initialized!', '', 'Commerce_Cursus Plugin');
-        }
+if (class_exists($className)) {
+    $handler = new $className($modx, $scriptProperties);
+    if (get_class($handler) == $className) {
+        $handler->run();
     } else {
-        $modx->log(xPDO::LOG_LEVEL_ERROR, $className. ' was not found!', '', 'Commerce_Cursus Plugin');
+        $modx->log(xPDO::LOG_LEVEL_ERROR, $className . ' could not be initialized!', '', 'Commerce_Cursus Plugin');
     }
+} else {
+    $modx->log(xPDO::LOG_LEVEL_ERROR, $className . ' was not found!', '', 'Commerce_Cursus Plugin');
 }
 
 return;
