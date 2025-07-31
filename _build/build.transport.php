6e10cc12228e006f2650c57b9e6abd4d3417e51e
Thomas Jakobi <thomas.jakobi@partout.info>
2025-07-31 13:00:53 +0200

    Fix a missing 'Plugins' array in the build script

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -150,6 +150,19 @@ $attr = [
             xPDOTransport::UPDATE_OBJECT => true,
             xPDOTransport::UNIQUE_KEY => 'name',
         ],
+        'Plugins' => [
+            xPDOTransport::PRESERVE_KEYS => true,
+            xPDOTransport::UPDATE_OBJECT => true,
+            xPDOTransport::UNIQUE_KEY => 'name',
+            xPDOTransport::RELATED_OBJECTS => true,
+            xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
+                'PluginEvents' => [
+                    xPDOTransport::PRESERVE_KEYS => true,
+                    xPDOTransport::UPDATE_OBJECT => false,
+                    xPDOTransport::UNIQUE_KEY => ['pluginid', 'event'],
+                ],
+            ],
+        ],
     ]
 ];
 
